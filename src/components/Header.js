28c1b0ed119990e29fd2f29f6ef674b320a70e78
Shaharina Mita <S.mita90232@gmail.com>
2022-07-17 12:43:11 +0600

    header added

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../images/Logo.svg';
+
+const Header = () => {
+    return (
+        <nav>
+            <img src={logo} alt='logo' />
+        </nav>
+    );
+};
+
+export default Header;
