df14163f2ffc9f5159521c33329c3659e3923c23
Shaharina Mita <S.mita90232@gmail.com>
2022-07-18 23:52:24 +0600

    style added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from '../../images/Logo.svg';
 
-import './Header.css/Header.css';
+import './Header.css';
 const Header = () => {
     return (
         <nav className='header'>
