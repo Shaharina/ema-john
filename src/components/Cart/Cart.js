2ac0dde1417fd07c0e325aa8a9844509ad6e6bd2
Shaharina Mita <S.mita90232@gmail.com>
2022-07-22 17:15:37 +0600

    parseFloat added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import './Cart.css';
 
-const Cart = ({ cart }) => {
+const Cart = props => {
+    const { cart } = props;
+    console.log(cart);
+
+    let total = 0;
+    let shipping = 0;
+
+    for (const product of cart) {
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    const tax = parseFloat((total * 0.1).toFixed(2));
+    const grandTotal = total + shipping + tax;
+
     return (
         <div className='cart'>
             <h1 className='order'>Order summary</h1>
             <p>Selected Items: {cart.length}</p>
-            <p>Total price: </p>
-            <p>Total Shipping: </p>
-            <p>Tax: </p>
-            <h5>Grand total</h5>
+            <p>Total price: $ {total} </p>
+            <p>Total Shipping: $ {shipping} </p>
+            <p>Tax: {tax} </p>
+            <h5>Grand total{grandTotal.toFixed(2)}</h5>
         </div>
     );
 };
