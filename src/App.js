c6fb1a35b72123c0785f97cf151eb90a1c271f04
Shaharina Mita <S.mita90232@gmail.com>
2022-07-29 13:19:36 +0600

    s

--- src/App.js
+++ src/App.js
@@ -11,11 +11,10 @@ function App() {
             <Header></Header>
             <Routes>
                 <Route path='/' element={<Shop></Shop>}></Route>
-                <Route path='/home' element={<Shop></Shop>}></Route>
+                <Route path='/shop' element={<Shop></Shop>}></Route>
                 <Route path='/orders' element={<Orders></Orders>}></Route>
                 <Route path='/inventory' element={<Inventory></Inventory>}></Route>
             </Routes>
-            <Shop></Shop>
         </div>
     );
 }
--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,9 +3,7 @@ import React from 'react';
 const Inventory = () => {
     return (
         <div>
-            <h1>
-                Inventory this <isindex />
-            </h1>
+            <h1>Inventory this is</h1>
         </div>
     );
 };
