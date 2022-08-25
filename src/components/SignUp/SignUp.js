c0ee78fd212a37d25899ea59b941c036082ecb3c
Shaharina Mita <S.mita90232@gmail.com>
2022-08-25 20:22:40 +0600

    signup added

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -9,6 +9,7 @@ const SignUp = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    s;
     const [error, setError] = useState('');
     const navigate = useNavigate();
 
