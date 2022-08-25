2f934f8caa79a7a30d8c300b82e3e82fa758db1e
Shaharina Mita <S.mita90232@gmail.com>
2022-08-25 20:22:55 +0600

    signup added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,18 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
 import logo from '../../images/Logo.svg';
 
 import './Header.css';
 const Header = () => {
+    const [user] = useAuthState(auth);
+
+    const handleSignOut = () => {
+        signOut(auth);
+    };
+
     return (
         <nav className='header'>
             <a href='/shop'>
@@ -16,7 +25,7 @@ const Header = () => {
                 <Link to='/inventory'>Inventory</Link>
                 <Link to='/about'>About</Link>
                 <Link to='/login'>Login</Link>
-                <Link to='/about'>About</Link>
+                {user ? <button onclick={handleSignOut}> Sign out</button> : <Link to='/about'>About</Link>}
             </div>
         </nav>
     );
